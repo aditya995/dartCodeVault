9a19612cf6605e339526be6d18d531e5951c4ac0
aditya995 <39437959+aditya995@users.noreply.github.com>
2021-10-04 02:25:20 +0600

    All git necessary command added.

--- Core_concepts/Print.dart
+++ Core_concepts/Print.dart
@@ -7,6 +7,6 @@ import 'dart:io';
 void main() {
   print('Line break added,');
   print('Line break added.');
-  stdout.write('side by side printed, ');
-  stdout.write('side by side printed. ');
+  stdout.write('side by side printed,');
+  stdout.write('side by side printed.');
 }
