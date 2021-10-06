ae06caaa6dd1b647f35949128e16af76c5732c05
aditya995 <39437959+aditya995@users.noreply.github.com>
2021-10-06 13:26:53 +0600

    015. file added
    Logical operators in dart

--- Core_concepts/010.operators.dart
+++ Core_concepts/010.operators.dart
@@ -7,16 +7,26 @@ void main() {
   double valC;
   valA = 2;
   valB = 3;
+
+  /// addition operation
   int res = valA + valB;
   print(res);
+
+  /// subtraction operation
   res = valA - valB;
   print(res);
+
+  /// Multiplication operation
   res = valA * valB;
   print(res);
   //res = valA / valB;  // Cannot assign double values to int variable
   ///will produce error in line:16
+
+  /// Divide operation
   valC = valA / valB;
   print(valC);
-  res = valB % valA; // ex. 3%2 = 1
+
+  /// Modulus operation
+  res = valB % valA; // e.g.(example given) 3%2 = 1
   print(res);
 }
