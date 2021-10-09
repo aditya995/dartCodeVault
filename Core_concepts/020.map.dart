6b1666b9299564b47de3c28b938a4e21f1b977b8
aditya995 <adityashowrov1996@gmail.com>
2021-10-10 03:10:44 +0600

    020.maps file added

---
+++ Core_concepts/020.map.dart
@@ -0,0 +1,8 @@
+/*
+dart 020.map.dart
+*/
+
+void main() {
+  var map = {1: 2, 2: '3', '4': '3'};
+  print(map);
+}
--- Core_concepts/023.generics_rune.dart
+++ Core_concepts/023.generics_rune.dart
@@ -27,11 +27,22 @@ void main() {
   });
 
   /// Generics
-  // It explicitly specifies the data-type for dynamic storages
+  // It explicitly specifies the data-type for some storages
 
   // Lists can potentially take multiple types of data
   var list1 = [1, 2, 't', '8', 4];
   // To make it specific use generics
   // Now it will not compile if list2 contains other than String !!
   var list2 = <String>['a', 'c', 'e', 'r'];
+
+  // maps can be specified too for certain datatypes
+  var map1 = {1: 'e', '3': 4}; //multiple datatypes in keys and values
+  var map2 = <int, String>{
+    1: 'w',
+    2: 'r'
+  }; // keys can only be int and values can only be string now!!
+
+  //
+  var set1 = {'a', 3, 'w'}; // multiple datatypes in values!!
+  var set2 = <String>{'a', 'r'}; //Only strings allowed
 }
