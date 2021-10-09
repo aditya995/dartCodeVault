e19f6a61952514a7580ad3d541e1cc4044c4d75c
aditya995 <adityashowrov1996@gmail.com>
2021-10-10 02:51:30 +0600

    019.list file added

---
+++ Core_concepts/019.list.dart
@@ -0,0 +1,45 @@
+/*
+dart 019.list.dart
+*/
+
+void main() {
+  /// Creating list
+  var list1 = [1, 2, 3, 2, 'ee', 1, 2, 3, 5];
+  var list2 = [9, 8, 7, 8];
+  print('$list1');
+
+  //list1.length; //returns a number
+  /// Access individual items
+  list1.first = 3; // value assigned
+  list1.last = 1;
+  print(
+      '$list1\nFirst element ${list1.first} \nElement at index 3-> ${list1.elementAt(3)}\nLast element ${list1.last}');
+
+  /// add items to list
+  //list1.add(value);
+  list1.add(4); // added at last
+  list1.addAll(list2); // takes another list and adds it at last
+  //list1.addAll(list1); // can't add itself, throughs error
+  list1.insert(1, 99); // adds element after that index
+  list1.insertAll(1, list2);
+  print(list1);
+
+  ///remove items form list
+  //list1.remove(2); // takes out first "2"
+  //list1.removeAt(1);  //removes 1th index value
+
+  ///find values
+  print(list1.contains(2));
+  list1.elementAt(1); // input index, outputs value
+  print(list1.indexOf(2));
+  print(list1.lastIndexOf(2));
+  var search = list1.indexOf(2, 2); // finds 2, starts from 2th index
+  print('finds element 2, starts from 2th index: 2 is at index "${search}"');
+
+  /// converts into map and stores it in map1
+  var map1 = list2.asMap();
+  print(map1);
+  print(list2); // list2 is still list, map1 gets assigned as map
+  var list3 = map1.values.toList(); // maps to list
+  print('$list3');
+}
