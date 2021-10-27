448f0d181e4b7a2e2e6951880a035cfca72cf6e3
aditya995 <39437959+aditya995@users.noreply.github.com>
2021-10-27 11:10:33 +0600

    updated

--- Core_concepts/027.class.dart
+++ Core_concepts/027.class.dart
@@ -2,15 +2,27 @@
 dart 027.class.dart
 */
 
-///class
+///class, access modifiers
 
 class MyClass {
+  /// there is no public, private, protected modifier
+  int publicVar = 20;
+  // identifiers that start with an underscore (_) are visible only inside the library.
+  // Every Dart app is a library, even if it doesn’t use a library directive.
+  int _privateVar = 10;
+
   void info() {
-    print('This is the class method');
+    print('Class method. \npublicVar = $publicVar, privateVar = $_privateVar');
   }
 }
 
 void main() {
   MyClass firstInstance = new MyClass();
+
+  ///Class properties
+  firstInstance._privateVar = 4; // still acts as a public storage
+  firstInstance.publicVar = 8;
+
+  /// Class methods
   firstInstance.info();
 }
