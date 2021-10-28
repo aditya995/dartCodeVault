02fa816015e7b617603a4947697f13ef5a63cb3f
aditya995 <adityashowrov1996@gmail.com>
2021-10-29 03:16:42 +0600

    updated

--- Core_concepts/032.static.dart
+++ Core_concepts/032.static.dart
@@ -1,12 +1,18 @@
 /**
 dart 032.static.dart
 */
-class Animal {
+abstract class Animal {
   int legs;
-  static int countAnimal = 0;
+  static int countAnimal = 0; //  Static property
   Animal(this.legs) {
-    print('Parent created/called');
+    print('##   Parent created/called');
     countAnimal++;
+  }
+
+  /// Static function
+  static int countInstance() {
+    //  Static function can only have static properties
+    return countAnimal;
   }
 
   void info() {
@@ -16,20 +22,32 @@ class Animal {
 }
 
 class Horse extends Animal {
+  static int countHorse = 0; //  Static property
   Horse({required int legs}) : super(legs) {
-    print('Child created');
+    print('**   Child created');
+    countHorse++;
   }
 }
 
 class Dove extends Animal {
-  Dove({required int legs}) : super(legs) {}
+  static int countDove = 0; //  Static property
+  Dove({required int legs}) : super(legs) {
+    print('**   Child created');
+    countDove++;
+  }
 }
 
 void main() {
+  print(
+      "Number of Animal objects = countInstance() -> ${Animal.countInstance()}, countAnimal = ${Animal.countAnimal}");
   Horse tedy = Horse(legs: 4);
   tedy.info();
   Dove stuart = Dove(legs: 2);
   stuart.info();
   Dove rocky = Dove(legs: 2);
   rocky.info();
+  print(
+      "Number of Animal objects = countInstance() -> ${Animal.countInstance()}, countAnimal = ${Animal.countAnimal}");
+  print(
+      "Number of Horse objects = ${Horse.countHorse}\nNumber of Dove objects = ${Dove.countDove}");
 }
