2529d8603bccdc8bf10efb98ebaeb1f12fc42d91
aditya995 <adityashowrov1996@gmail.com>
2021-10-31 19:05:49 +0600

    Files added

--- Core_concepts/ShiftOperators.dart
+++ Core_concepts/ShiftOperators.dart
@@ -2,5 +2,14 @@
 dart ShiftOperators.dart
 */
 
-/// Shifting Operators
-void main() {}
+/// Shifting Operators  >>, << ,<=, >=
+
+void main() {
+  int a = 13; //  Binary 101
+  print((a).toRadixString(2));
+  print((a >> 1).toRadixString(2));
+  print((a << 3).toRadixString(2));
+  print(a.toRadixString(2));
+  a <<= 3; //  Value changed
+  print(a.toRadixString(2));
+}
