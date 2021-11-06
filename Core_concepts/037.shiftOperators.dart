fa59a50e268e44e92140c57bed859ad001340cb0
aditya995 <adityashowrov1996@gmail.com>
2021-11-06 21:34:01 +0600

    Comments updated

--- Core_concepts/037.shiftOperators.dart
+++ Core_concepts/037.shiftOperators.dart
@@ -6,10 +6,10 @@ dart 037.shiftOperators.dart
 
 void main() {
   int a = 13; //  Binary 101
-  print((a).toRadixString(2) + '\n');
-  print((a >> 1).toRadixString(2));
-  print((a << 3).toRadixString(2));
-  print(a.toRadixString(2));
+  print((a).toRadixString(2) + '\n'); //  1101
+  print((a >> 1).toRadixString(2)); //  110
+  print((a << 3).toRadixString(2)); //  1101000
+  print(a.toRadixString(2)); //  1101
   a <<= 3; //  Value changed
-  print(a.toRadixString(2));
+  print(a.toRadixString(2)); //  1101000
 }
